8342b21a9ea09d1c4b9e308de95761a7aa7d2654
Alex Semenov <aleksey.semenov.fin@gmail.com>
2018-11-06 09:37:49 +0300

    Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookingAppStore.Models;
+using System.Data.Entity;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо, " + purchase.Person + ", за покупку!";
+        }
+    }
+}
---
+++ Models/BookContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BookingAppStore.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            context.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+            context.Books.Add(new Book { Name = "Отцы и дети", Author = "Тургенев", Price = 180 });
+            context.Books.Add(new Book { Name = "Война и мир", Author = "А. Чехов", Price = 150 });
+
+            base.Seed(context);
+        }
+    }
+}
